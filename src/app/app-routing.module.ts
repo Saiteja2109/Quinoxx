651458728d66f781c2cea105de13cf22483c8144
Saiteja2109 <tejaguptha60@gmail.com>
2022-11-08 16:51:11 +0530

    Qunioxx Task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WebsiteComponent } from './website/website.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'website', component:WebsiteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
